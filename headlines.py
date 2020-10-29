918350bf47e0923b15c9dd71d74c2712250616d3
are <areifeoluwa10@gmail.com>
2020-10-29 19:53:51 +0100

    added weather features

--- headlines.py
+++ headlines.py
@@ -1,7 +1,11 @@
+import os
 import feedparser
+from dotenv import load_dotenv
 from flask import Flask
 from flask import render_template, request
+import requests
 
+load_dotenv()
 app = Flask(__name__)
 
 RSS_FEED = {
@@ -11,7 +15,6 @@ RSS_FEED = {
     "iol":"http://www.iol.co.za/cmlink/1.640"
 }
 
-
 @app.route("/")
 def get_news(publication="bbc"):
     query = request.args.get("publication")
@@ -20,9 +23,27 @@ def get_news(publication="bbc"):
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEED[publication])
+    weather = get_weather("Lagos,Nigeria")
     return render_template(
-        "home.html", articles=feed["entries"]
+        "home.html", articles=feed["entries"], weather=weather
     )
+
+def get_weather(query):
+    api_url = "http://api.openweathermap.org/data/2.5/weather"
+    r = requests.get(api_url, params={
+        "appid":os.getenv("WEATHER_API_KEY"),
+        "units":"metric",
+        "q":query
+    }).json()
+    weather = None
+    if r.get("weather"):
+        weather = {
+            "description": r["weather"][0]["description"],
+            "temperature": r["main"]["temp"],
+            "city":r["name"]
+        }
+    return weather
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
