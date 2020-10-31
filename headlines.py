7127fb741c0ed6cc2e64164e3025e968c7a32d61
are <areifeoluwa10@gmail.com>
2020-10-31 21:39:35 +0100

    added caching.

--- headlines.py
+++ headlines.py
@@ -1,8 +1,9 @@
 import os
+import datetime
 import feedparser
 from dotenv import load_dotenv
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, make_response
 import requests
 
 load_dotenv()
@@ -22,33 +23,41 @@ DEFAULTS = {
     "currency_to":"USD"
 }
 
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
+
+
 @app.route("/")
 def home():
     # Get headlines, based on user input
-    publication = request.args.get("publication")
-    if not publication:
-        publication = DEFAULTS["publication"]
+    publication = get_value_with_fallback("publication")
     articles = get_news(publication)
     # Get weather from location based on user input
-    city = request.args.get("city")
-    if not city:
-        city = DEFAULTS["city"]
+    city = get_value_with_fallback("city")
     weather = get_weather(city)
     # Get currencies to be converted from user
-    currency_from = request.args.get("currency_from")
-    currency_to = request.args.get("currency_to")
-    if not currency_from:
-        currency_from = DEFAULTS["currency_from"]
-    if not currency_to:
-        currency_to = DEFAULTS["currency_to"]
+    currency_from = get_value_with_fallback("currency_from")
+    currency_to = get_value_with_fallback("currency_to")
     rate, currencies = get_rate(currency_from, currency_to)
 
-    return render_template(
+    response = make_response(render_template(
         "home.html", articles=articles,
-        weather=weather, currency_from=currency_from,
-        currency_to=currency_to, rate=rate,
+        weather=weather,
+        currency_from=currency_from,
+        currency_to=currency_to, 
+        rate=rate,
         currencies=sorted(currencies)
-    )
+    ))
+    expires = datetime.datetime.now() + datetime.timedelta(days=365)
+    response.set_cookie("publication", publication, expires=expires)
+    response.set_cookie("city", city, expires=expires)
+    response.set_cookie("currency_from", currency_from, expires=expires)
+    response.set_cookie("currency_to", currency_to, expires=expires)
+    return response
 
 
 def get_news(query):
@@ -83,6 +92,7 @@ def get_rate(frm, to):
     frm_rate = r.get(frm.upper())
     to_rate = r.get(to.upper())
     return (round(to_rate/frm_rate, 3), r.keys())
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
